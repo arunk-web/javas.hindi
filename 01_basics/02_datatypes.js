7fc802c8de1966b7cf1290acbf20b8ad34a578f3
arunk-web <arunk02615@gmail.com>
2025-08-02 13:35:25 +0530

    datatype

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,9 +6,12 @@
 
 console.log("arun"); console.log("kumar")  //ye shi hai but not a goood practise 
 console.log("arun")  // aise likhna haii for good practise
-console.log("kumar")
+console.log("kumar")  
 
 
+let name = "arun" // string ho jayega 
+let age = 18 //integer data type
+let isLoggedIn = false   //boolean value ho gyi
 
 //number => 2 to power 53
 // bigint     (for larger numbers)
@@ -16,3 +19,11 @@ console.log("kumar")
 // boolean => true/false;
 // null => standalone valuee   , null value hm assign bhi kr skte hai 
 // undefined => only value declare not assigned any value yet 
+// symbol = unique
+
+
+
+console.log(typeof "arun");   // for finding the type
+
+
+
