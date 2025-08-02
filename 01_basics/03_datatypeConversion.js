3828744670bc2f77adf3e1e85f64abad5f5c3e2a
arunk-web <arunk02615@gmail.com>
2025-08-02 16:12:51 +0530

    conversion of datatypes

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,36 @@
+// datatype conversion
+let score = 33  //now its datatype is Number
+
+let score1 = "33" // string
+
+// console.log(typeof score1);
+
+// ab muhje jaise convert krna haii (string to number)
+let valueInNumber = Number(score1)
+
+// console.log(typeof(valueInNumber));
+
+
+//"33" => 33
+// "33abc" => NaN
+// true converted to 1 and false converted to 0;
+
+
+// important
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 converted to true,,,,0 converted to false
+// case of empty string => false answer
+// "hitesh" =>true  (string me aise kuch hoo)
+
+
+
+
+
+
+
+
+
+
