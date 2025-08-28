1f4468def7061268593c05cd436fc89290b1cb3c
arunk-web <arunk02615@gmail.com>
2025-08-28 19:51:15 +0530

    control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,4 +1,5 @@
-//if 
+//if  ***************************************lec-26**********************
+//in this lecture we will try to control the code according to the situation
 const isuser = true;
  // if else condition phle ki trh hi honge 
 if(2 === "2"){    //agr no. ke saath saath hme data type bhi same chahiye toh use triple equal
@@ -123,16 +124,29 @@ if(Object.keys(emptyobject).length === 0){
     console.log("not empty");
 }
 
+//**********remember */
+// false == 0 true
+// false == '' true
+// 0 == '' true
 
+//Nullish coalescing operator (??) : null undefined   (BETWEEN TWO)
 
+let val1;
+val1 = 5 ?? 10   //first value jo milegi wo assign ho jayegii
+console.log(val1);    //ans : 5
 
+val1 = null ?? 10       //null ke baad phli value 10 hai,here it means agr koi 3rd party null return krti hai toh hm wo nhi lena chahte toh usko rewrite kr do
+console.log(val1);    //ans : 10
 
+val1 = undefined ?? 15   //phli value 15 haiiii
+console.log(val1);     //ans : 15
 
+//terniary operator
+// condition ?  true : false
 
-
-
-
-
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+;
 
 
 
