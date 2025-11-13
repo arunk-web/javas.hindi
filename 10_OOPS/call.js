fb808f3215653390adf4c1f59b21623d851429b0
arunk-web <arunk02615@gmail.com>
2025-11-14 02:10:17 +0530

    call

--- 10_OOPS/call.js
+++ 10_OOPS/call.js
@@ -1,14 +1,16 @@
-function setUsername(username){
+// call and this in js  (call bind appply)
+
+function setUsername(username){     //like hm koi application (discord) bna rhe hai tohh uska username set kr rhe hai toh uska code aisa hova
     //complex DB calls
     this.username = username;
 }
 
 function createUser(username,email,password){
-    setUsername(username)
+    setUsername.call (this,username)        //bina call ke hmne sirf usks referrence diya hai usko call ni kiya hai jiske liye call use krna hai hme
 
     this.email = email;
     this.password = password
 }
 
 const chai = new createUser("chai","chai@fb.com","123")
-console.log(chai)
+console.log(chai)    //aise normally email password set ho jaynge
